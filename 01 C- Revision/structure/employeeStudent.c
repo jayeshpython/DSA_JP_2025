5aa5dff239699005f56a4499769ca00d8de57492
jayeshpython <jayeshdpatil2021@gmail.com>
2025-02-24 23:55:12 +0530

    structure assignment

---
+++ 01 C- Revision/structure/employeeStudent.c
@@ -0,0 +1,67 @@
+
+
+#include <stdio.h>
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+} Date;
+
+typedef struct {
+    char companyName[30];
+    int empId;
+    double salary;
+    int experience;
+} Employee;
+
+typedef struct {
+    char name[30];
+    int age;
+    Date dob;  
+    char bloodGroup[5];
+    Employee emp; 
+} Person;
+
+int main() {
+    Person p;
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", p.name);
+
+    printf("Enter Age: ");
+    scanf("%d", &p.age);
+
+    printf("Enter Date of Birth (DD MM YYYY): ");
+    scanf("%d %d %d", &p.dob.day, &p.dob.month, &p.dob.year);
+
+    printf("Enter Blood Group: ");
+    scanf("%s", p.bloodGroup);
+
+    printf("Enter Company Name: ");
+    scanf(" %[^\n]", p.emp.companyName);
+
+    printf("Enter Employee ID: ");
+    scanf("%d", &p.emp.empId);
+
+    printf("Enter Salary: ");
+    scanf("%lf", &p.emp.salary);
+
+    printf("Enter Years of Experience: ");
+    scanf("%d", &p.emp.experience);
+
+   
+    printf("\n********** Person Details **********\n");
+    printf("Name: %s\n", p.name);
+    printf("Age: %d\n", p.age);
+    printf("Date of Birth: %02d-%02d-%d\n", p.dob.day, p.dob.month, p.dob.year);
+    printf("Blood Group: %s\n", p.bloodGroup);
+
+    printf("\n********** Employee Details **********\n");
+    printf("Company Name: %s\n", p.emp.companyName);
+    printf("Employee ID: %d\n", p.emp.empId);
+    printf("Salary: %.2lf\n", p.emp.salary);
+    printf("Years of Experience: %d\n", p.emp.experience);
+
+    return 0;
+}
---
+++ 01 C- Revision/structure/item.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Item{
+    char itemName[20];
+    int quantity;
+    int price;
+    double amount;
+}it;
+
+void accept(it* ptr);
+double calculateAmount(it* ptr);
+void display(it* ptr);
+
+int main(){
+     it *ptr = (it*)malloc(sizeof(it));
+    double Amount;
+
+    accept(ptr);
+    ptr->amount = calculateAmount(ptr);
+    display(ptr);
+
+    free(ptr);
+    return 0;
+}
+
+void accept(it* ptr){
+    printf("Enter item name: ");
+    scanf("%s", ptr->itemName);
+
+    printf("Enter quantity: ");
+    scanf("%d", &ptr->quantity);
+
+    printf("Enter price: ");
+    scanf("%d", &ptr->price);
+}
+
+double calculateAmount(it* ptr){
+    return ptr->quantity * ptr->price;
+}
+
+void display(it* ptr){
+    printf("\nItem details");
+    printf("\nItem name: %s", ptr->itemName);
+    printf("\nQuantity: %d", ptr->quantity);
+    printf("\nPrice: %d", ptr->price);
+    printf("\nAmount: %lf", ptr->amount);
+}
+
+
+
